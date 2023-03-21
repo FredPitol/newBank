780e926890be3ab55214429a093920347b9ac7a0
Frederico Pitol <freirefrederico1@gmail.com>
2023-03-21 13:17:15 -0300

    Adding values to atributes

--- CreateAccount.java
+++ CreateAccount.java
@@ -4,5 +4,14 @@ public class CreateAccount {
        ClientAccount firstAccount = new ClientAccount(); 
        firstAccount.balance = 200;
        System.out.println(firstAccount.balance);
+        
+       firstAccount.balance = firstAccount.balance + 100;
+       System.out.println(firstAccount.balance); 
+
+       ClientAccount secondAccount = new ClientAccount();
+       secondAccount.balance = 50;
+       
+       System.out.println("The first account have " + firstAccount.balance );
+       System.out.println("The second account have " + secondAccount.balance);
     }
 }
